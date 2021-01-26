3c1bbfc090e4bb20d1a03a6ce6dbfbeff968ed1c
araujo-p <araujo@byui.edu>
2021-01-26 11:02:38 -0300

    Assignment Portal update - accessibility and js

--- js/currentdate.js
+++ js/currentdate.js
@@ -4,3 +4,7 @@ document.getElementById('currentDateTime').textContent = new Date().toLocaleDate
 //Current year
 var y = new Date();
 document.getElementById('currentyear').textContent = y.getFullYear();
+
+//Last updated information
+var update = document.lastModified;
+document.getElementById('currentDateTime').innerHTML = update;
