437524a24ddf52cb1d30a47485a1f19b20902af0
araujo-p <araujo@byui.edu>
2021-02-19 02:57:01 -0300

    Preston W5 js and media query updated

---
+++ lesson5/js/display-msg.js
@@ -0,0 +1,22 @@
+/* Defining Table
+ * Input: current day, month and year from user's computer clock
+ * Processing: Check if it's Friday
+ * Output: Display the massage to the user
+ */
+
+function friday() {
+var message;
+ //Get information from user's computer clock
+ var now = new Date();
+ var dayOfWeek = now.getDay();
+
+ //Evaluate the days and display message
+ if (dayOfWeek == 5) {
+    message = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion."
+}
+
+//Display message to the user
+document.getElementById("friday-mgs").textContent = message;
+document.getElementById("friday-mgs").style.display = "block";
+
+}
