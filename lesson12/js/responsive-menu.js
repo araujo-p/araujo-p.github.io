e720caa3f7850980306a2885599598a6bb59089d
araujo-p <araujo@byui.edu>
2021-04-05 00:40:14 -0300

    header and footer created for all pages

---
+++ lesson12/js/responsive-menu.js
@@ -0,0 +1,5 @@
+//Function toggleMenu for smaller views
+function toggleMenu() {
+    console.log(document.getElementById("navbar").classList);
+    document.getElementById("navbar").classList.toggle("responsive");
+}
---
+++ lesson12/js/update-info.js
@@ -0,0 +1,10 @@
+//Current date
+//document.getElementById('currentDateTime').textContent = new Date().toLocaleDateString('en-US');
+
+//Current year
+//var y = new Date();
+//document.getElementById('currentyear').textContent = y.getFullYear();
+
+//Last updated information
+var update = document.lastModified;
+document.getElementById('currentDateTime').innerHTML = update;
